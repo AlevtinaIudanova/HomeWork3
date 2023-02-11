b57e7a50ea75dc676416bdd305ac469b47bde11f
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-11 18:28:07 +0100

    3

---
+++ HomeWork33/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Написать программу вычисления произведения чисел от 1 до N*/
+
+Console.WriteLine("Enter number: ");
+int A = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= A; i++)
+{
+    result *= i;
+    Console.WriteLine("Факториал \"{0}\" = {1}", i, result);
+
+}
