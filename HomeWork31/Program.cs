e2fe1975001f0f7d9e2ad406745a81308edf38b5
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-11 18:24:59 +0100

    1

---
+++ HomeWork31/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Найти кубы чисел от 1 до N*/
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i} \t {i * i * i} ");
+}
