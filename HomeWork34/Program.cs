b57bda49fc7b9d445def6990a74dbc51c7e59685
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-11 18:29:19 +0100

    4

---
+++ HomeWork34/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Показать кубы чисел, заканчивающихся на четную цифру*/
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    int temp = i * i * i;
+    if (temp % 2 == 0)
+    {
+        Console.WriteLine($"{i} \t {i * i * i} <==");
+    }
+    else
+    {
+        Console.WriteLine("-");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
