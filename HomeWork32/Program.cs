11e9b5e334718bba65b31691a980741f19cc8f73
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-11 18:26:35 +0100

    2

---
+++ HomeWork32/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Подсчитать сумму цифр в числе*/
+
+Console.Write("Enter a number: ");
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+
+while (a > 0)
+{
+    s = s + a % 10;
+    a = a / 10;
+}
+Console.WriteLine(s);
